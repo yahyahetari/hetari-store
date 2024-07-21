b2cf2a14264bdb5f4067d85c8f5966e5843535b2
yahyahetari <132639815+yahyahetari@users.noreply.github.com>
2024-07-21 17:20:28 +0300

    next url

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         },
       ],
     },
+    env: {
+      MONGODB_URL: process.env.MONGODB_URL,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
   };
   
   export default nextConfig;
