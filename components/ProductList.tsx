5e3afa9f73cd9750bb0610d8ab7182cf945a66eb
yahyahetari <132639815+yahyahetari@users.noreply.github.com>
2024-07-22 17:28:04 +0300

    product

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -12,7 +12,7 @@ const ProductList = async () => {
       ) : (
         <div className="flex flex-wrap justify-center gap-16">
           {products.map((product: ProductType) => (
-            <ProductCard key={product._id} product={product}/>
+            <ProductCard key={product._id} product={product}  />
           ))}
         </div>
       )}
